d9e9c7db6b95d1c95c3e67c68e5beaba1f716be7
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-07-13 15:42:41 -0700

    new task script added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
+#include <unidstd.h>
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Description: write(int fd, const void *buf, size_t count)
+ *
+ * Return: Always 1 (Fail)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58)
-	return (0);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
