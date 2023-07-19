8cbc1dcca6cbc72fe03fb290c1a88ce912fecd3e
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-07-19 15:12:56 -0700

    new task script added

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 1; i < 1024; ++i)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
+
+	printf("%ld", sum);
+
+	return (0);
+}
