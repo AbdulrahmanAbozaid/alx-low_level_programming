18b8c29ed0b1825c86273993ee7e161ed4202bc3
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-07-18 18:22:39 -0700

    new task script added

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,6 +10,12 @@
 
 int print_last_digit(int n)
 {
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	int lastDigit;
+
+	if (n < 0)
+		lastDigit = -1 * (n % 10);
+	else 
+		lastDigit = n % 10;
+	_putchar(lastDigit + '0');
+	return (lastDigit);
 }
