3616a5735947888ca0cb26322a76b512b2589d69
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-07-18 18:16:22 -0700

    new task script added

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - pritns the digit of n
+ * @n: number
+ *
+ * Return: last digit
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
