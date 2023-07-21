f688f0b2df589d85ed639023a1108888ffe23048
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-07-20 19:02:53 -0700

    new task script added

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - is upper of c
+ * @c: char
+ *
+ * Return: integer
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'B')
+	{
+		return 1;
+	}
+
+	return 0;
+}
