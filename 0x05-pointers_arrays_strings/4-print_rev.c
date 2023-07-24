92b0e4efa0558f0b662afea7517d8a4476c65f92
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-07-24 16:54:00 -0700

    new task script added

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - print
+ * @s: str
+ *
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	char *c;
+	char a = *s;
+	int ctr = -1;
+
+	for (c = s; *c != '\n'; c++)
+	{
+		ctr++;
+	}
+
+	for (c = s + ctr; c != s - 1; c--)
+	{
+		printf("%c", *c);
+	}
+
+	putchar('\n');
+}
