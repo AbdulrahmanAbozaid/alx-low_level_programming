7828f4644fa1bb0711724b73a648d6370612bb18
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-07-24 17:40:24 -0700

    new task script added

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - fgsdf
+ * @str: gdsfgh
+ *
+ * Return: gsdfhgds
+ */
+
+void puts_half(char *str)
+{
+	int len = 0, i;
+
+	while (str[len])
+		len++;
+
+	if (len % 2 == 0)
+		i = len / 2;
+	else
+		i = (len - 1) / 2;
+
+	for (; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
