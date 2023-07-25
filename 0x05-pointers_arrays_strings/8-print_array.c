2256061d3cd88565d61d26471656550ed36d5556
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-07-24 17:49:50 -0700

    new task script added

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - gsdg
+ * @a: edst
+ * @n: size
+ *
+ * Return: fgesdgse
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; ++i)
+	{
+		printf("%d", a[i]);
+
+		if (i != n - 1)
+			printf(", ");
+	}
+	_putchar('\n');
+}
