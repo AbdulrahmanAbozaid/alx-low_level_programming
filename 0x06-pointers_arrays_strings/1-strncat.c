86975c12979046ce3732c405be77b3aaf8918c54
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-07-26 16:33:23 -0700

    new task script added

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,18 +11,13 @@
 
 char *_strncat(char *dest, char *src, int n);
 {
-	int i = 0, j = 0;
+	int i = 0, j;
 
 	while (dest[i])
 		i++;
 
-	while (src[j])
-		j++;
-
-	if (n > j)
-		n = j;
-
-	for (j = 0; j < n; ++j)
-		dest[i++] = src[j];
+	for (j = 0; j < n && src[j] != '\0'; ++j)
+		dest[i + j] = src[j];
+	dest[i + j] = '\0';
 	return (dest);
 }
