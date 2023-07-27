bd2e6845acacb6b004c243228e5b3a5231894471
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-07-26 17:10:16 -0700

    new task script added

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strncpy - gdsg
+ * @dest: gdsg
+ * @src: gr
+ * @n: num
+ *
+ * Return:gsdgets
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
