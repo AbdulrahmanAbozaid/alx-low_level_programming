c337e46abc024b78ca77194c76dd841a1e845fbc
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-07-28 08:19:42 -0700

    new task script added

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * leet - gsdg
+ * @s: gsdgs
+ *
+ * Return: gsdg
+ */
+
+char *leet(char *s)
+{
+	int i, j;
+
+	for (i = 0; s[i]; ++i)
+	{
+		if (s[i] == 'a' || s[i] == 'A')
+			s[i] = '4';
+		else if (s[i] == 'e' || s[i] == 'E')
+			s[i] = '3';
+		else if (s[i] == 'o' || s[i] == 'O')
+			s[i] = '0';
+		else if (s[i] == 't' || s[i] == 'T')
+			s[i] = '7';
+		else if (s[i] == 'l' || s[i] == 'L')
+			s[i] = '1';
+	}
+	return (s);
+}
