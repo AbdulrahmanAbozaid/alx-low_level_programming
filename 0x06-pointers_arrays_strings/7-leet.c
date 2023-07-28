6f7378930d9180bb6089a8aa2c24492293bccfae
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-07-28 08:28:36 -0700

    new task script added

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,20 +10,19 @@
 
 char *leet(char *s)
 {
-	int i, j;
+	char *cp = s;
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int val[] = {4, 3, 0, 7, 1};
+	unsigned int i;
 
-	for (i = 0; s[i]; ++i)
+	while (*c)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		else if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		else if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		else if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		else if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (i = 0; i < sizeof(key) / sizeof(char); ++i)
+		{
+			if (*c == key[i] || *c == key[i] + 32)
+				*c = 48 + val[i];
+		}
+		c++;
 	}
-	return (s);
+	return (cp);
 }
