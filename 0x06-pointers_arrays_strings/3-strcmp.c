ddf22c19c7558fa65667dca0321fda674a693c4a
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-07-27 19:26:35 -0700

    new task script added

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,34 +11,17 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, b = 1, len1 = 0, len2 = 0;
+	int eq = 0;
 
-	while (s1[len1])
-		len1++;
-	while (s2[len2])
-		len2++;
-
-	for (i = 0; s1[i] && s2[i]; ++i)
+	while (*s1)
 	{
-		if (s1[i] != s2[i])
+		if (*s1 != *s2)
 		{
-			b = 0;
+			eq = *s1 - *s2;
 			break;
 		}
+		s1++;
+		s2++;
 	}
-
-	if (b == 0 || len1 != len2)
-	{
-		i = 0;
-
-		if (s1[0] != s2[0])
-			while (s1[0] == s2[0])
-				i++;
-		if (s1[i] > s2[i])
-			return (1);
-		else
-			return (-1);
-	}
-	else
-		return (0);
+	return (eq);
 }
