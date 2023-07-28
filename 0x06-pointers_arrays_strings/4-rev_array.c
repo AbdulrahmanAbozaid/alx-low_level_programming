503c4d5872203d24c80151db34ffdc288b311ce4
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-07-27 19:36:44 -0700

    new task script added

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -13,10 +13,10 @@ void reverse_array(int *a, int n)
 {
 	int i, j = n / 2, tmp;
 
-	for (i = 0; i <= j; ++i)
+	for (i = 0, j = (n - 1); i < j; ++i, --j)
 	{
 		tmp = a[i];
-		a[i] = a[n - i - 1];
-		a[n - i - 1] = tmp;
+		a[i] = a[j];
+		a[j] = tmp;
 	}
 }
