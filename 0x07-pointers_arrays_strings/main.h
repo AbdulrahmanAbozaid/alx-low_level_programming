927d1d558c076d47189f76df66dd229db3b87ad6
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-08-01 16:37:56 -0700

    new task script added

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+char *string_toupper(char *);
+char *leet(char *);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *cap_string(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
+#endif
