ca1d9256762c5fa8c23365781d824ef0e9505028
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-08-01 18:30:45 -0700

    new task script added

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strstr - sdg
+ * @haystack: frsewarw
+ * @needle: gewger
+ *
+ * Return: gresgseder
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i, j;
+
+	for (i = 0; haystack[i] != '\0'; ++i)
+	{
+		if (haystack[i] == needle[0])
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if ()
+		}
+	}
+}
