f0a0117d8f3c4a7af08ce8e98b845c4f61593403
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-08-01 17:19:28 -0700

    new task script added

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,13 +12,15 @@
 char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
+	char *p;
 
 	for (i = 0; s[i] != '\0'; ++i)
 	{
 		for (j = 0; accept[j] == '\0'; j++)
 		{
+			p = &s[i];
 			if (accept[j] == s[i])
-				return (s + i);
+				return (p);
 		}
 	}
 
