d12b65572cf52a800c43c1bf964ab9deebe69021
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-08-04 08:44:44 -0700

    new task script added

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _print_rev_recursion - greyert
+ * @s: fgesdgfsd
+ *
+ * Return: grghsdfghsfd
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	s++;
+	_print_rev_recursion(s);
+	_putchar(*s);
+}
