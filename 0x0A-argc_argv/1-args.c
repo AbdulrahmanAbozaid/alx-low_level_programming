db17f3edf4d8bad9f5457b4f25e2850be8a2b827
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-08-04 17:44:08 -0700

    new task script added

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: gewsg
+ * @argv: gsg
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argc);
+	return (0);
+}
