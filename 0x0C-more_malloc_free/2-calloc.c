00982f9fb95c0747324754c5d9c29aa6855f8e83
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-08-10 17:59:55 -0700

    new task script added

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - ghresger
+ * @nmemb: gewgtewsg
+ * @size: hrsgrsdgt
+ * Return: hgrsgsgs
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *m;
+	int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	m = malloc(nmemb * size);
+
+	if (m == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		(int)m[i] = 0;
+
+	return (m);
+}
