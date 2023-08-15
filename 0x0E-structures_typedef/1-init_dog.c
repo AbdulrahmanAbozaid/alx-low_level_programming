4170f43cdf175fe67fc906685793c53bba2210e7
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-08-15 16:02:45 -0700

    new task script added

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * init_dog - gdsfds
+ * @d: gegsd
+ * @name: fsafsf
+ * @age: sgdsg
+ * @owner: fsdgsdf
+ * Return: sgsdgdsg
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	
+}
