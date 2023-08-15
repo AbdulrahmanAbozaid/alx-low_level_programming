29e2e7434bbd7cbdc17b3c9c7c6621c26917f58a
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-08-15 16:35:15 -0700

    new task script added

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,7 +5,7 @@
  * print_dog - gsdgsdgs
  * @d: gsdgsdgs
  *
- * Return: sdgdsgdsg
+ * Return: void
  */
 
 void print_dog(struct dog *d)
@@ -14,6 +14,6 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: %s\n", d->name ? d->name : "(nil)");
 		printf("Age: %f\n", d->age);
-		printf("Name: %s\n", d->owner ? d->owner : "(nil)");
+		printf("Owner": %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
