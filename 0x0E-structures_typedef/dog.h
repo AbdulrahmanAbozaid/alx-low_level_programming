024c224da0a2099658203670849df94199c38e91
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-08-15 16:06:24 -0700

    new task script added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef _DOG_H
 #define _DOG_H
 
+/**
+ * struct dog - sdg
+ * @name: fagsd
+ * @age: gsgsd
+ * @owner: gsgsd
+ * Description: gsdgsd
+ */
+
 struct dog {
 	char *name;
 	float age;
