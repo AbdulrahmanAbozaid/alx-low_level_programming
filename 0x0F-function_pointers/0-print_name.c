3915fe7a9ecf9f92dd39c768257207bceddabf6f
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-08-16 20:00:28 -0700

    new task script added

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_name - sgdfdsg
+ * @name: fsgsdf
+ * @f: ssgsd
+ *
+ * Return: gddsgds
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		(*f)(name);
+
+}
