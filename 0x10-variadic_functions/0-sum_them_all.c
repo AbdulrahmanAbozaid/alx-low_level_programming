9120991532b8555a34bdf4297576ff44df0dec14
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-08-17 16:59:25 -0700

    new task script added

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,16 +11,15 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int i, sum = 0;
-	va_list args;
+	int i = n, sum = 0;
+	va_list ag;
 
 	if (n == 0)
 		return (0);
-	va_start(args, n);
+	va_start(ag, n);
+	while (i--)
+		sum += va_arg(ag, int);
 
-	for (i = 0; i < n; ++i)
-		sum += va_arg(args, int);
-
-	va_end(args);
+	va_end(ag);
 	return (sum);
 }
