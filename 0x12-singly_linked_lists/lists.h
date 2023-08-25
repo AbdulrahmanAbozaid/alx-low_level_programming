efb12af401a5b1378904a279824ba1eedd964e6b
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-08-25 05:21:20 -0700

    new task script added

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 
 /**
  * struct list_s - singly linked list
@@ -12,9 +15,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 int _putchar(char c);
