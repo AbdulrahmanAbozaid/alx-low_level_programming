65cefed7157c1fc7b934da0f8c9b3ae7aa0829f2
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-08-25 06:25:18 -0700

    new task script added

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * _constructor - before main
+ * Return: void
+ */
+
+void _constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
