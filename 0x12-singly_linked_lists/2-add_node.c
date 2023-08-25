042c188780d934d8b703d83a35e5440eb94e887c
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-08-25 05:36:44 -0700

    new task script added

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * add_node - add to list
+ * @head: head
+ * @str: s
+ *
+ * Return: ptr
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *pl = malloc(sizeof(list_t));
+
+	if (pl == NULL || !head)
+		return (NULL);
+	if (str)
+	{
+		pl->str = strdup(str);
+		if (!pl->str)
+		{
+			free(pl);
+			return (NULL);
+		}
+
+		pl->len = _strlen(str);
+	}
+
+	pl->next = *head;
+	*head = pl;
+
+
+	return (pl);
+}
