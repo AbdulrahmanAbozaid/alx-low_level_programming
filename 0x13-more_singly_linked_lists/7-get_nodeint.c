5ef10a6411e5265dffde758b1975206ccbbd61a6
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-08-29 12:07:19 -0700

    new task script added

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,19 +13,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *node;
 	int i = 0;
 
-	if (!head)
-		return (NULL);
-
-	node = head;
-
-	while (node && i < index)
-	{
-		node = node->next;
-		i++;
-	}
-
-	if (i + 1 < index)
-		return (NULL);
+	for (node = head, node && i < index; node = node->next, i++)
+		;
 
 	return (node);
 }
