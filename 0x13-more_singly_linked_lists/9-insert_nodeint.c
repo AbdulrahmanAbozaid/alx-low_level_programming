5959fd03628511e2ffda6b420d313aec2aece7de
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-08-29 12:26:53 -0700

    new task script added

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - fss
+ * @head: fas
+ * @idx: ffse
+ * @n: fafa
+ *
+ * Return: fafaf
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *nod, newNod = malloc(sizeof(listint_t));
+	unsigned int i = 0;
+
+	if (!head || !newNod)
+		return (NULL);
+
+	newNod->n = n;
+	newNod->next = NULL;
+
+	if (!idx)
+	{
+		newNod->next = *head;
+		*head = newNod;
+		return (newNod);
+	}
+
+	nod = *head;
+	while (node)
+	{
+		if (i == idx - 1)
+		{
+			newNod->next = nod->next;
+			node->next = newNod;
+			return (newNod);
+		}
+		i++;
+		nod = nod->next;
+	}
+	free(newNod);
+	return (NULL);
+}
