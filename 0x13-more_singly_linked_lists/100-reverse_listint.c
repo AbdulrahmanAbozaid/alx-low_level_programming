2f3c2f0ad280d42bc4a8e398b66f0d823dba8c4d
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-08-30 04:55:20 -0700

    new task script added

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - safa
+ * @head: g
+ *
+ * Return: fafs
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node = NULL, *tmp = NULL;
+
+	if (!head || !*head)
+		return (NULL);
+
+	node = *head;
+	*head = NULL;
+	while (node)
+	{
+		tmp = node->next;
+		node->next = *head;
+		*head = node;
+		node = tmp
+	}
+	return (*head);
+}
