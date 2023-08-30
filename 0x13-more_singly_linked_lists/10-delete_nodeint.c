444f77211d511ebbe37062c220dbdbc6cf806b30
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-08-30 04:43:20 -0700

    new task script added

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - fsafas
+ * @head: faagas
+ * @index: fsaf
+ *
+ * Return: ffss
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *node, *tmp;
+	unsigned int i;
+
+	if (!head || !*head)
+		return (-1);
+
+	node = *head;
+
+	if (!index)
+	{
+		node = *head;
+		*head = node->next;
+		free(node);
+		return (1);
+	}
+
+	while (node)
+	{
+		if (i == index)
+		{
+			tmp->next = node->next;
+			free(node);
+			return (1);
+		}
+
+		i++;
+		tmp = node;
+		node = node->next;
+	}
+
+	return (-1);
+}
