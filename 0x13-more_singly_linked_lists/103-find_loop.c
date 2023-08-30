a64db4cacaf8b63e37653ac8b86e3790f7b806c1
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-08-30 06:14:09 -0700

    new task script added

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -9,18 +9,18 @@
 
 listint_t *find_listint_loop(listint_t *head)
 {
-        listint_t *pn, *end;
+	listint_t *pn, *end;
 
-        if (head == NULL)
-                return (NULL);
+	if (head == NULL)
+		return (NULL);
 
-        for (end = head->next; end != NULL; end = end->next)
-        {
-                if (end == end->next)
-                        return (end);
-                for (pn = head; pn != end; pn = pn->next)
-                        if (pn == end->next)
-                                return (end->next);
-        }
-        return (NULL);
+	for (end = head->next; end != NULL; end = end->next)
+	{
+		if (end == end->next)
+			return (end);
+		for (pn = head; pn != end; pn = pn->next)
+			if (pn == end->next)
+				return (end->next);
+	}
+	return (NULL);
 }
