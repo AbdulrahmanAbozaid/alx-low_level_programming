da7312750fb87ed3751b939eb03fbdc36845280a
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-08-30 04:45:28 -0700

    new task script added

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,9 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i;
 
 	if (!head || !*head)
-		retu	
-			node = *head;rn (-1);
-
+		return (-1);
 
 	if (!index)
 	{
