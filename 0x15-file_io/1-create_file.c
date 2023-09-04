e7441c9f453ee11ff3e11e928a39d8f547599264
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-09-04 15:23:29 -0700

    new task script added

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -35,7 +35,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	file = open(filename, O_WRONLY | O_CREATE | O_TRUNC, S_IRUSR | S_IWUSR);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file == -1)
 		return (-1);
 	if (len)
