0ecb88e4880ed5837c760b1770f14fbc627d569b
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-09-04 15:16:29 -0700

    new task script added

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,7 +31,7 @@ int _strlen(char *s)
 int create_file(const char *filename, char *text_content)
 {
 	int file;
-	ssize_t size, len = _strlen(text);
+	ssize_t size, len = _strlen(text_content);
 
 	if (!filename)
 		return (-1);
