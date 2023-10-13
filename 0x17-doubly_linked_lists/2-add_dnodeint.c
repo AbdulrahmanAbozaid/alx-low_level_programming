1299cc23a8aa6a3294d3146aec1f8df0b76b0e87
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-10-13 15:58:59 +0300

    fas

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *node = malloc(sizeof(dlistint_t));
 
 	if (node == NULL || !head)
-		return (node ? node, NULL : NULL);
+		return (node ? free(node), NULL : NULL);
 	node->n = n;
 	node->next = NULL;
 	node->prev = NULL;
