f79bf204e48ad3a8a585dc1ef5c212849f9a3551
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-10-13 15:21:30 +0300

    fas

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists"
+
+/**
+ * print_dlistint - gsgsg
+ * @h: fafasf
+ * Return: afasf
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t ctr = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		ctr++;
+	}
+
+	return (ctr);
+}
