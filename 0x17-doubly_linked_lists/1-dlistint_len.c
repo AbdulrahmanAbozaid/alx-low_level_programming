9da520ec6784d736a4d5f27551047533dee6bc6e
AbdulrahmanAbozaid <diver72003@gmail.com>
2023-10-13 15:27:55 +0300

    fas

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,13 +8,13 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-        size_t ctr = 0;
+	size_t ctr = 0;
 
-        while (h)
-        {
-                h = h->next;
-                ctr++;
-        }
+	while (h)
+	{
+		h = h->next;
+		ctr++;
+	}
 
-        return (ctr);
+	return (ctr);
 }
