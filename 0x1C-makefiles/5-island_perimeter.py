34d2c3e494abd3a7fc4e502b4ccb45134f3687b5
AbdulrahmanAbozaid <diver72003@gmail.com>
2024-03-21 06:21:19 +0200

    CMake

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,5 +1,31 @@
 #!/usr/bin/python3
 """Solve Technical Interview Quws"""
 
+
+def count_edges(grid, i, j, rows, cols):
+    """Calc the number of water edges"""
+    up, down = (i - 1, j), (i + 1, j)
+    left, right = (i, j - 1), (i, j + 1)
+    sm = 4
+    for k in (up, down, left, right):
+        if (rows > k[0] >= 0) and (cols > k[1] >= 0):
+            sm -= grid[k[0]][k[1]] == 1
+
+    return sm
+
+
 def island_perimeter(grid):
     """Calc The Perimeter"""
+
+    if not grid:
+        return 0
+
+    rows = len(grid)
+    cols = len(grid[0])
+    sm = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            sm += count_edges(grid, i, j, rows, cols) if grid[i][j] == 1 else 0
+
+    return sm
